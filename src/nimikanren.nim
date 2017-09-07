e9103d6742bed61ee0360c2603d283ead1abba8c
jaccarmac <jaccarmac@gmail.com>
2017-09-07 02:43:55 +0000

    Refactor tests.
    FossilOrigin-Name: 27b97d43ed77d7af8bb6a81d179de3bc1fbc25c3

--- src/nimikanren.nim
+++ src/nimikanren.nim
@@ -1,3 +1,9 @@
 type
   LogicVar* = tuple
     name: string
+
+proc initLogicVar*(name: string): LogicVar =
+  (name: name)
+
+proc `$`*(v: LogicVar): string =
+  v.name
--- tests/all.nim
+++ tests/all.nim
@@ -2,12 +2,14 @@ import unittest
 import nimikanren
 
 suite "Represent logic variables.":
-  test "Create a new LogicVar.":
-    let x: LogicVar = (name: "x")
-    check x.name == "x"
-  test "Compare LogicVars.":
-    let x1: LogicVar = (name: "x")
-    let x2: LogicVar = (name: "x")
-    let y1: LogicVar = (name: "y")
-    check x1 == x2
-    check x1 != y1
+  test "Create a named logic variable.":
+    let v = initLogicVar "x"
+    check $v == "x"
+  test "Compare similar logic variable.":
+    let v1 = initLogicVar "x"
+    let v2 = initLogicVar "x"
+    check v1 == v2
+  test "Compare dissimilar logic variables.":
+    let x = initLogicVar "x"
+    let y = initLogicVar "y"
+    check x != y
